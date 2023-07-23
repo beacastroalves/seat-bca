209cbdb3d40673922e4a192f57d47078539ef65d
beatriz alves <beacastro1008@gmail.com>
2023-07-23 17:43:25 +0100

    refactor: adjust the imports

--- src/main/java/com/github/beacastroalves/seatbca/models/User.java
+++ src/main/java/com/github/beacastroalves/seatbca/models/User.java
@@ -1,7 +1,10 @@
 package com.github.beacastroalves.seatbca.models;
 
-import jakarta.persistence.*;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity(name = "users")
 public class User {
