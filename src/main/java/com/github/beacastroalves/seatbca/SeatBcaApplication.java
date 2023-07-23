7a4b3b58b8aa15a9324d92b929285589bd26782c
beatriz alves <beacastro1008@gmail.com>
2023-07-23 14:46:49 +0100

    initial commit  Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/github/beacastroalves/seatbca/SeatBcaApplication.java
@@ -0,0 +1,13 @@
+package com.github.beacastroalves.seatbca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatBcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatBcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/beacastroalves/seatbca/SeatBcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.beacastroalves.seatbca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatBcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
